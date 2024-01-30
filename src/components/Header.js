89ca675ed20eab778b8abf5f92a95ad6b2827c00
Sraddha Anil <sraddha>
2024-01-31 03:15:33 +0530

    116 commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,132 +1,23 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import logoImage from "../images/logo.png";
-// import "../styles/Header.css";
-
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//   faUser,
-//   faHeart,
-//   faShoppingBag,
-//   faSearch,
-// } from "@fortawesome/free-solid-svg-icons";
-
-// function Header() {
-//   const [searchQuery, setSearchQuery] = useState("");
-
-//   const handleSearchChange = (e) => {
-//     setSearchQuery(e.target.value);
-//     // Add your search logic here, for example, navigating to search results
-//     // console.log("Search query:", e.target.value);
-//   };
-
-//   return (
-//     <div className="header">
-//       <div className="content">
-//         <nav className="navbar navbar-expand-lg navbar-dark">
-//           <div className="container-fluid d-flex justify-content-around">
-//             <Link className="navbar-brand nav-link active" to="/">
-//               <img src={logoImage} alt="Logo" className="logo" />
-//             </Link>
-//             <button
-//               className="navbar-toggler"
-//               type="button"
-//               data-bs-toggle="collapse"
-//               data-bs-target="#navbarNav"
-//               aria-controls="navbarNav"
-//               aria-expanded="false"
-//               aria-label="Toggle navigation"
-//             >
-//               <span className="navbar-toggler-icon"></span>
-//             </button>
-//             <div className="collapse navbar-collapse" id="navbarNav">
-//               <ul className="navbar-nav">
-//                 <li className="nav-item">
-//                   <Link to="/men" className="nav-link">
-//                     MEN
-//                   </Link>
-//                 </li>
-//                 <li className="nav-item">
-//                   <Link to="/women" className="nav-link">
-//                     WOMEN
-//                   </Link>
-//                 </li>
-//                 <li className="nav-item">
-//                   <Link to="/kids" className="nav-link">
-//                     KIDS
-//                   </Link>
-//                 </li>
-//                 <li className="nav-item">
-//                   <Link to="/beauty" className="nav-link">
-//                     BEAUTY
-//                   </Link>
-//                 </li>
-//               </ul>
-
-//               <form className="d-flex ms-auto search-container position-relative">
-//                 <FontAwesomeIcon icon={faSearch} className="search-icon" />
-//                 <input
-//                   className="form-control me-2 pl-4 search-input"
-//                   type="search"
-//                   placeholder="Search for products, brands and more"
-//                   aria-label="Search"
-//                   value={searchQuery}
-//                   onChange={handleSearchChange}
-//                   style={{ width: "400px" }}
-//                 />
-//               </form>
-//               <div className="icons-container">
-//                 <Link to="/profile">
-//                   <FontAwesomeIcon
-//                     icon={faUser}
-//                     style={{ color: "white", marginLeft: "10px", cursor: "pointer" }}
-//                   />
-//                 </Link>
-
-//                 <Link to="/wishlist">
-//                   <FontAwesomeIcon
-//                     icon={faHeart}
-//                     style={{ color: "white", marginLeft: "10px", cursor: "pointer" }}
-//                   />
-//                 </Link>
-
-//                 <Link to="/cart">
-//                   <FontAwesomeIcon
-//                     icon={faShoppingBag}
-//                     style={{ color: "white", marginLeft: "10px", cursor: "pointer" }}
-//                   />
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-//         </nav>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Header;
-
-
-
-
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios"; // Import Axios
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faUser, faHeart, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faUser,
+  faHeart,
+  faShoppingBag,
+} from "@fortawesome/free-solid-svg-icons";
 import logoImage from "../images/logo.png";
 import "../styles/Header.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   // State to manage the search query and search results
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   const handleSearchChange = async (e) => {
     const query = e.target.value;
@@ -164,8 +55,7 @@ function Header() {
               data-bs-target="#navbarNav"
               aria-controls="navbarNav"
               aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
+              aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
@@ -210,25 +100,27 @@ function Header() {
                       <Link
                         key={result.id}
                         to={`/search/${searchQuery}`}
-                        className="search-result"
-                      >
+                        className="search-result">
                         {result.name}
                       </Link>
                     ))}
                   </div>
-                )} 
+                )}
+                
               </form>
               <div className="icons-container">
-              {!localStorage.getItem("currentUser") &&<Link to="/profile">
-                  <FontAwesomeIcon
-                    icon={faUser}
-                    style={{
-                      color: "white",
-                      marginLeft: "10px",
-                      cursor: "pointer",
-                    }}
-                  />
-                </Link>}
+                {!localStorage.getItem("currentUser") && (
+                  <Link to="/profile">
+                    <FontAwesomeIcon
+                      icon={faUser}
+                      style={{
+                        color: "white",
+                        marginLeft: "10px",
+                        cursor: "pointer",
+                      }}
+                    />
+                  </Link>
+                )}
 
                 <Link to="/wishlist">
                   <FontAwesomeIcon
@@ -251,13 +143,18 @@ function Header() {
                     }}
                   />
                 </Link>
-                {localStorage.getItem("currentUser") &&<button className="mx-3 btn btn-light"  onClick={()=>{
-                  localStorage.removeItem("currentUser")
-                  setTimeout(()=>{
-                    navigate("/profile")
-
-                  },2000)
-                }}>Logout</button>}
+                {localStorage.getItem("currentUser") && (
+                  <button
+                    className="mx-3 btn btn-light"
+                    onClick={() => {
+                      localStorage.removeItem("currentUser");
+                      setTimeout(() => {
+                        navigate("/profile");
+                      }, 2000);
+                    }}>
+                    Logout
+                  </button>
+                )}
               </div>
             </div>
           </div>
